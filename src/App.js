b0cf52d8cec4cc6727e516856b1ac17a2f24d7fd
ahcock <63637115+ahcock@users.noreply.github.com>
2021-02-15 15:15:50 +0900

    feat: apply routing to app 1. applied basic routing with react-router-dom library 1. 기본적인 라우팅이 react-router-dom 라이브러리를 이용해 적용되었습니다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepages/homepage.component';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <Homepage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route path="/toprics/:topicId" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
