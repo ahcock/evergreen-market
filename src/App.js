b1b8951b945c3b77b1557fd734e3aff672c30596
ahcock <63637115+ahcock@users.noreply.github.com>
2021-02-15 23:37:14 +0900

    feat: Make Header component 1. added Header component 1. Header 콤포넌트가 만들어졌습니다.
    Header component has Logo, shop, contact elements. All three of them are routed via <Link> tag.
    Logo goes to index page, shop and contact go to /shop url. contact route path will be changed
    
    헤더 콤포넌트는 Logo, shop, contact 엘레멘트를 가지고 있습니다. 셋 모두는 Link태그로 인해 라우팅 되어있고, Logo는 인덱스 페이지로, shop, contact는 /shop url로 이동하게 됩니다.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepages/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 import './App.css';
 
 const HatsPage = () => (
@@ -13,6 +14,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
