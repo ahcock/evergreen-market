e63ec1e9136a53bcea6c83dd557b9686903aa6f9
ahcock <63637115+ahcock@users.noreply.github.com>
2021-02-16 13:51:29 +0900

    feat: Make Signin-Signup page 1. made signin & signup page 2. added signin component
    1. 로그인, 회원가입 페이지를 만들어라
    2. 로그인, 회원가입 페이지에 로그인 콤포넌트를 만들었음.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepages/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import './App.css';
 
 const HatsPage = () => (
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
         <Route exact path="/shop/hats" component={HatsPage} />
       </Switch>
     </div>
