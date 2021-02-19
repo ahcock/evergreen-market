af8eed4ce4d21faf526fee83918c44974f1a28e5
ahcock <63637115+ahcock@users.noreply.github.com>
2021-02-19 13:48:10 +0900

    feat: Apply reslect library to cart-icon, cart-dropdown component 1. Reselect library is applied to cart-icon and cart-dropdown component to memoize total item count in the cart 1. cart-icon, cart-dropdown 콤포넌트의 카트안 아이템의 총 개수를 momoize된 값을 쓰기 위해, Reselect 라이브러리가 도입, 사용되었습니다

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+import { selectCartItemsCount } from '../../redux/cart/cart.reselect';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, quantity }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{quantity}</span>
   </div>
 );
+
+// 카트 아이콘의 숫자를 업데이트 할 수 있는 로직
+const mapStateToProps = (state) => ({
+  quantity: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.reselect.js
@@ -0,0 +1,14 @@
+import { createSelector } from 'reselect';
+
+// 여기서 state는 root redecer의 state전체를 얘기하는 것이다.
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+);
