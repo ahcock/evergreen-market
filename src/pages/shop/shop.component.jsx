cccf4be4b65ab466846940342ffe5ca51090f1a8
ahcock <63637115+ahcock@users.noreply.github.com>
2021-02-24 16:53:30 +0900

    Refactoring: Implment Redux-thunk 1. Implemented Redux-thunk, and Asyncronous collection data fetch logic is now moved into shop reducer, fetch logic is dispached by redux-thunk middleware 2. Applied Container Pattern to Shop-page component & Collection Component 1. 리덕스 사가가 적용되었습니다. Firebase로 부터 데이터를 가져오는 로직이, shop-reducer안으로 옮겨졌고, redux-thunk에 의해 dispatch 됩니다. 2. Shop-page 콤포넌트와Collection-Overview(Shop Container에서 쓰임) & Collection 콤포넌트에 Container-Pattern이 사용되었습니다.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { updateCollections } from '../../redux/shop/shop.actions';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
-
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-// import CollectionPage from '../collection/collection.component';
-import CollectionPage from '../collection/collection.component';
-// import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container';
+import CollectionPageContainer from '../collection/collection.component';
 
 class ShopPage extends React.Component {
   state = {
@@ -25,35 +13,22 @@ class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection('collections');
-
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionMap);
-        this.setState({ loading: false });
-      }
-    );
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
   }
 
   render() {
     const { match } = this.props;
-    const { loading } = this.state;
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
-          render={(props) => (
-            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
-          )}
+          component={CollectionsOverviewContainer}
         />
         <Route
           path={`${match.path}/:collectionId`}
-          render={(props) => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
-          )}
+          component={CollectionPageContainer}
         />
       </div>
     );
@@ -61,8 +36,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,14 +2,28 @@ import ShopActionsType from './shop.types';
 
 const INITIAL_STATE = {
   collections: null,
+  isFetching: false, // reducer 다른 부분들이 data가 fetch 중인지 판단하는 요소로 작용
+  errorMessage: undefined,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopActionsType.UPDATE_COLLECTIONS:
+    case ShopActionsType.FETCH_COLLECTIONS_START:
       return {
         ...state,
+        isFetching: true,
+      };
+    case ShopActionsType.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
         collections: action.payload,
+      };
+    case ShopActionsType.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload, // error message string
       };
     default:
       return state;
